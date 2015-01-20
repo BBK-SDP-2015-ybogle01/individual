56f99115a6be35abacc47e438371297fbbb21003
byakov <yakov.boglev@gmail.com>
2015-01-20 21:33:09 +0000

    Added skeleton of BnzInstruction class

---
+++ sml/src/sml/BnzInstruction.java
@@ -0,0 +1,20 @@
+package sml;
+
+/**
+ * Created by byakov on 20/01/2015.
+ */
+public class BnzInstruction extends Instruction {
+    public BnzInstruction(String l, String op) {
+        super(l, op);
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
