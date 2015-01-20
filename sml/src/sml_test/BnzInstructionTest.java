5ae6ec76281c99443f6af1269414283d6487a31b
byakov <yakov.boglev@gmail.com>
2015-01-20 21:34:41 +0000

    Added skeleton of BnzInstruction test class

---
+++ sml/src/sml_test/BnzInstructionTest.java
@@ -0,0 +1,25 @@
+package sml_test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class BnzInstructionTest {
+    MachineMock m;
+
+    @Before
+    public void setUp() {
+        m = new MachineMock();
+    }
+
+    @Test
+    public void testExecute() {
+        fail();
+    }
+
+    @Test
+    public void testToString() {
+        fail();
+    }
+}
