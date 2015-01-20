ecef29cf9498e5508d25f43f44ddcf1b147a8c8d
byakov <yakov.boglev@gmail.com>
2015-01-20 17:40:16 +0000

    Created test class for AddInstruction Created a mock machine to use with this and other tests.

---
+++ sml/src/sml_test/AddInstructionTest.java
@@ -0,0 +1,28 @@
+package sml_test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddInstructionTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testExecute() {
+
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+}
---
+++ sml/src/sml_test/MachineMock.java
@@ -0,0 +1,12 @@
+package sml_test;
+
+import sml.Machine;
+import sml.Registers;
+
+/**
+ * Created by byakov on 20/01/2015.
+ */
+
+public class MachineMock extends Machine {
+    Registers registers = new Registers();
+}
