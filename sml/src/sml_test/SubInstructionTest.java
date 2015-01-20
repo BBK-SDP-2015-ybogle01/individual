3411441ea70dbe1475ea19046974ed4df3e2f86e
byakov <yakov.boglev@gmail.com>
2015-01-20 18:44:40 +0000

    Added one more test case (simple subtraction)

--- sml/src/sml_test/SubInstructionTest.java
+++ sml/src/sml_test/SubInstructionTest.java
@@ -3,6 +3,9 @@ package sml_test;
 import org.junit.Before;
 import org.junit.Test;
 import sml.Machine;
+import sml.SubInstruction;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by byakov on 20/01/2015.
@@ -22,5 +25,14 @@ public class SubInstructionTest {
         m.getRegisters().setRegister(11,3);
         new SubInstruction("L1", 5, 10, 11).execute(m);
         assertEquals(2, m.getRegisters().getRegister(5));
+    }
+
+    @Test
+    public void testExecuteWithNegativeResult() {
+        // 10 - 300 into reg-27
+        m.getRegisters().setRegister(1,10);
+        m.getRegisters().setRegister(21,300);
+        new SubInstruction("L2", 27, 1, 21).execute(m);
+        assertEquals(-290, m.getRegisters().getRegister(27));
     }
 }
