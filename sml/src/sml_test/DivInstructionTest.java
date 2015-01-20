c695e292931380b9298506e9cbe9a2f63c9bd310
byakov <yakov.boglev@gmail.com>
2015-01-20 20:48:05 +0000

    Added outline of test for DivInstruction class

---
+++ sml/src/sml_test/DivInstructionTest.java
@@ -0,0 +1,22 @@
+package sml_test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DivInstructionTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
