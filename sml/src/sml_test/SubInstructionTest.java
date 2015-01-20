95a12718330ef59d0fb4642abb0c457191d49251
byakov <yakov.boglev@gmail.com>
2015-01-20 18:49:55 +0000

    Added toString test case

--- sml/src/sml_test/SubInstructionTest.java
+++ sml/src/sml_test/SubInstructionTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import sml.Machine;
 import sml.SubInstruction;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by byakov on 20/01/2015.
@@ -34,5 +34,13 @@ public class SubInstructionTest {
         m.getRegisters().setRegister(21,300);
         new SubInstruction("L2", 27, 1, 21).execute(m);
         assertEquals(-290, m.getRegisters().getRegister(27));
+    }
+
+    @Test
+    public void testToString() {
+        m.getRegisters().setRegister(0,10);
+        m.getRegisters().setRegister(30,20);
+        String s = new SubInstruction("L3", 20, 0, 30).toString();
+        assertTrue("result was " + s, "L3: sub 0 - 30 to 20".equals(s));
     }
 }
