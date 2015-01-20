3c175ee7fa8da549cf9c7db3bbc9e4c392572b53
byakov <yakov.boglev@gmail.com>
2015-01-20 15:57:41 +0000

    Initial commit

---
+++ sml/src/sml/LinInstruction.java
@@ -0,0 +1,30 @@
+package sml;
+
+/**
+ * Created by byakov on 19/01/2015.
+ */
+public class LinInstruction extends Instruction {
+    private int register;
+    private int value;
+
+    public LinInstruction(String label, String opcode) {
+        super(label, opcode);
+    }
+
+    public LinInstruction(String label, int register, int value) {
+        super(label, "lin");
+        this.register = register;
+        this.value = value;
+
+    }
+
+    @Override
+    public void execute(Machine m) {
+        m.getRegisters().setRegister(register, value);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " register " + register + " value is " + value;
+    }
+}
---
+++ sml/src/sml/Registers.java
@@ -0,0 +1,36 @@
+package sml;
+
+import lombok.Data;
+
+/**
+ * Created by byakov on 19/01/2015.
+ */
+@Data
+public class Registers {
+
+    private final static int NUMBEROFREGISTERS = 32;
+    private int registers[];
+
+    // Constructor: an instance whose registers are set to 0
+
+    {
+        registers = new int[NUMBEROFREGISTERS];
+    }
+
+    public Registers() {
+        for (int i = 0; i != registers.length; i++) {
+            registers[i] = 0;
+        }
+    }
+
+    // Set register i to v.
+    // Precondition: 0 <= i <= NUMBEROFREGISTERS
+
+    public void setRegister(int i, int v) {
+        registers[i] = v;
+    }
+
+    public int getRegister(int i) {
+        return registers[i];
+    }
+}
