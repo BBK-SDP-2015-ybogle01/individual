fe90e0c42e3a046f3e9cc381cc57d49a3754a69c
byakov <yakov.boglev@gmail.com>
2015-01-20 21:23:15 +0000

    Added outline of test class

---
+++ sml/src/sml_test/OutInstructionTest.java
@@ -0,0 +1,25 @@
+package sml_test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OutInstructionTest {
+    MachineMock m;
+
+    @Before
+    public void setUp() {
+        m = new MachineMock();
+    }
+
+    @Test
+    public void testExecute() {
+        fail();
+    }
+
+    @Test
+    public void testToString() {
+        fail();
+    }
+}
