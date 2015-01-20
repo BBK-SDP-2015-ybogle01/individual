f0d5ab4b4db35c31627538d06fd6341b9ee22704
byakov <yakov.boglev@gmail.com>
2015-01-20 21:03:31 +0000

    Added tests and execute method of DivInstr tests are normal division and divide by zero. Latter should throw ArithmeticExcep

--- sml/src/sml_test/DivInstructionTest.java
+++ sml/src/sml_test/DivInstructionTest.java
@@ -2,6 +2,10 @@ package sml_test;
 
 import org.junit.Before;
 import org.junit.Test;
+import sml.DivInstruction;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class DivInstructionTest {
     MachineMock m;
@@ -12,12 +16,24 @@ public class DivInstructionTest {
     }
 
     @Test
-    public void testExecute() throws Exception {
+    public void testExecuteDivide() throws Exception {
+        // 15 / 3 into reg-5
+        m.getRegisters().setRegister(10,15);
+        m.getRegisters().setRegister(11,3);
+        new DivInstruction("L1", 5, 10, 11).execute(m);
+        assertEquals(5, m.getRegisters().getRegister(5));
+    }
 
+    @Test (expected = ArithmeticException.class)
+    public void testExecuteDivideByZero() throws Exception {
+        // 15 / 0 into reg-5
+        m.getRegisters().setRegister(10,15);
+        m.getRegisters().setRegister(11,0);
+        new DivInstruction("L1", 5, 10, 11).execute(m);
     }
 
     @Test
     public void testToString() throws Exception {
-
+        fail();
     }
 }
