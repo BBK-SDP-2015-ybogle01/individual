279025276d683f5280b8385fb53f147a8612409d
byakov <yakov.boglev@gmail.com>
2015-01-20 19:01:23 +0000

    Added execute method Only changed the operation taking place, otherwise code is the same as the other instructions (+/-)

--- sml/src/sml/MulInstruction.java
+++ sml/src/sml/MulInstruction.java
@@ -21,6 +21,8 @@ public class MulInstruction extends Instruction {
 
     @Override
     public void execute(Machine m) {
-
+        int value1 = m.getRegisters().getRegister(op1);
+        int value2 = m.getRegisters().getRegister(op2);
+        m.getRegisters().setRegister(result, value1 * value2);
     }
 }
