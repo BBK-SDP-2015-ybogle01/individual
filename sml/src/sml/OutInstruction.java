7765aca4f8ed034cbc3d7c5759357faadd64b17b
byakov <yakov.boglev@gmail.com>
2015-01-20 21:21:09 +0000

    Added outline of OutInstr class This doesn't have to change any regs, just print to console

---
+++ sml/src/sml/OutInstruction.java
@@ -0,0 +1,27 @@
+package sml;
+
+/**
+ * Created by ybogle01 on 19/01/2015.
+ */
+public class OutInstruction extends Instruction {
+    private int register;
+
+    public OutInstruction(String label, String opcode) {
+        super(label, opcode);
+    }
+
+    public OutInstruction(String label, int register) {
+        super(label, "out");
+        this.register = register;
+    }
+
+    @Override
+    public void execute(Machine m) {
+        m.getRegisters().setRegister(register, value);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " register " + register + " value is " + value;
+    }
+}
