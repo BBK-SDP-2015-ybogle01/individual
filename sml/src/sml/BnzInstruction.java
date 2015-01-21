ff65028d532f01eb4baf90db9d8c210f244ed783
byakov <yakov.boglev@gmail.com>
2015-01-21 21:56:25 +0000

    Added execute code and test passed

--- sml/src/sml/BnzInstruction.java
+++ sml/src/sml/BnzInstruction.java
@@ -4,13 +4,23 @@ package sml;
  * Created by byakov on 20/01/2015.
  */
 public class BnzInstruction extends Instruction {
-    public BnzInstruction(String l, String op) {
-        super(l, op);
+    private int register;
+    private String branchLabel;
+
+    public BnzInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    public BnzInstruction(String label, int register, String branchLabel) {
+        this(label, "bnz");
+        this.register = register;
+        this.branchLabel = branchLabel;
     }
 
     @Override
     public void execute(Machine m) {
-
+        int test = m.getRegisters().getRegister(register);
+        if (test != 0) m.setPc(m.getLabels().indexOf(branchLabel));
     }
 
     @Override
--- sml/src/sml_test/BnzInstructionTest.java
+++ sml/src/sml_test/BnzInstructionTest.java
@@ -2,6 +2,7 @@ package sml_test;
 
 import org.junit.Before;
 import org.junit.Test;
+import sml.BnzInstruction;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -18,12 +19,12 @@ public class BnzInstructionTest {
     public void testExecuteIsNotZero() {
         // check reg-10 which isn't 0
         m.getRegisters().setRegister(10, 10);
+        m.getLabels().addLabel("Null-0");
         m.getLabels().addLabel("Null-1");
         m.getLabels().addLabel("Null-2");
-        m.getLabels().addLabel("Null-3");
         m.getLabels().addLabel("BranchTarget");
-        new BnzInstructionTest("L1", 10, "BranchTarget");
-        assertEquals(4, m.getPc());
+        new BnzInstruction("L1", 10, "BranchTarget").execute(m);
+        assertEquals(3, m.getPc());
     }
 
     @Test
